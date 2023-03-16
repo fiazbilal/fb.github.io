780029ba41c18c3488d7b0dba14a8802ec098753
fiazbilal <fiaz.bilal@mslm.io>
2023-03-16 17:43:26 +0500

    few initial changes

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ npm start
 
 # Run this for production build
 npm run build
+
+# Run for deployment
+git checkout -b gh-pages
+mv build/* ./
+git add .
+git commit -m "Deploy app to GitHub Pages"
+git push origin gh-pages
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, I am Muhammad Fiaz</h1>
+      <p><a href="fiaz.bilal1@gmail.com">fiaz.bilal1@gmail.com</a>.</p>
+    </div>
+  );
+}
+
+export default App;
